d97283f8407b50792ffbfd419f27019a8b94f6b1
Ryan Oliveira <ryan.p@ba.estudante.senai.br>
2024-10-27 19:19:54 -0300

    update

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
+
 const CourseCard = ({ title, description, image }) => {
     return (
         <div className="course-card">
