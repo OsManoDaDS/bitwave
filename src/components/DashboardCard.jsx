ce66d623362533f4dbfb37d29470a9b413571cd4
JV <joao.v.cunha6@ba.estudante.senai.br>
2024-11-06 15:05:45 -0300

    botão logout

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
 
-
 const DashboardCard = ({ title, description, onClick, rating, showRating }) => {
     const [userRating, setUserRating] = useState(rating || 0); // Armazena a avaliação do usuário
     const [notification, setNotification] = useState('');
     //const [myCourses, setMyCourses] = useState([]);
-
-    
 
     const handleRating = (value) => {
         setUserRating(value);
@@ -17,51 +14,52 @@ const DashboardCard = ({ title, description, onClick, rating, showRating }) => {
     };
 
     return (
-            <div className="dashboard-card" onClick={onClick}>
-                <h3>{title}</h3>
-                <p>{description}</p>
-                {showRating && (
-                    <div className="rating">
-                        {[1, 2, 3, 4, 5].map((star) => (
-                            <span
-                                key={star}
-                                className={`star ${star <= userRating ? 'filled' : ''}`}
-                                onClick={() => handleRating(star)}
-                            >
-                                ★
-                            </span>
-                        ))}
-                    </div>
-                )}
-                {showRating && <p>Avaliação: {userRating} estrela{userRating > 1 ? 's' : ''}</p>}
-                {notification && <p className="notification">{notification}</p>}
-            </div>
-
-            /*<div className='dashboard-card'>
-                <div>
-                    {myCourses.map((course, index) => (
-                        <div>
-                            <h3>{course.name}</h3>
-                            <p>{course.description}</p>
-                            {showRating && (
-                                <div className="rating">
-                                    {[1, 2, 3, 4, 5].map((star) => (
-                                        <span
-                                            key={star}
-                                            className={`star ${star <= userRating ? 'filled' : ''}`}
-                                            onClick={() => handleRating(star)}
-                                        >
-                                            ★
-                                        </span>
-                                    ))}
-                                </div>
-                            )}
-                            {showRating && <p>Avaliação: {userRating} estrela{userRating > 1 ? 's' : ''}</p>}
-                            {notification && <p className="notification">{notification}</p>}
-                        </div>
+        <div className="dashboard-card" onClick={onClick}>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            {showRating && (
+                <div className="rating">
+                    {[1, 2, 3, 4, 5].map((star) => (
+                        <span
+                            key={star}
+                            className={`star ${star <= userRating ? 'filled' : ''}`}
+                            onClick={() => handleRating(star)}
+                        >
+                            ★
+                        </span>
                     ))}
                 </div>
-            </div>*/
+            )}
+            {showRating && <p>Avaliação: {userRating} estrela{userRating > 1 ? 's' : ''}</p>}
+            {notification && <p className="notification">{notification}</p>}
+
+        </div>
+
+        /*<div className='dashboard-card'>
+            <div>
+                {myCourses.map((course, index) => (
+                    <div>
+                        <h3>{course.name}</h3>
+                        <p>{course.description}</p>
+                        {showRating && (
+                            <div className="rating">
+                                {[1, 2, 3, 4, 5].map((star) => (
+                                    <span
+                                        key={star}
+                                        className={`star ${star <= userRating ? 'filled' : ''}`}
+                                        onClick={() => handleRating(star)}
+                                    >
+                                        ★
+                                    </span>
+                                ))}
+                            </div>
+                        )}
+                        {showRating && <p>Avaliação: {userRating} estrela{userRating > 1 ? 's' : ''}</p>}
+                        {notification && <p className="notification">{notification}</p>}
+                    </div>
+                ))}
+            </div>
+        </div>*/
     );
 };
 
