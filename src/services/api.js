9c9724660b1720b7e4343bdef3bbad82bb995d18
Pedro Portales <pedroportales@riseup.net>
2024-11-06 15:57:49 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
 
-    // baseURL: 'https://api-crud-1-sqcl.onrender.com'
-    baseURL: 'http://localhost:8081'
+    baseURL: 'https://api-crud-1-sqcl.onrender.com'
+    //baseURL: 'http://localhost:8081'
 
 })
 
